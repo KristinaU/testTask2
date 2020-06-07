0139f476892dba02c4fe6370315dbc684668894b
KristinaU <urosova.k@gmail.com>
2020-06-07 01:46:56 +0100

    Try redis

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 from flask import Flask
 from flask import request
+import redis
 
 app = Flask(__name__)
 
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return "Hello"
 
-#registration
+
+# registration
 @app.route('/registration', methods=['POST'])
 def registration():
-#    return 'You are ' + request.form['username'] + ' ' + request.form['password']
-
     if createUser(request.form['username'], request.form['password']):
-        return 'yeah haaahhh', 200
-    else:
-        return 'Ohh nooo', 400
+        return r.get(1)
+
+#        if r.get(1) == 'username':
+#            return 'yeah haaahhh', 200
+#        else:
+#            return 'Something went wrong', 400
+#    else:
+#        return 'Ohh nooo', 400
+
 
 def createUser(username, password):
-    if (username == 'xxx'):
-        return False
-    else:
-        return True
+    r.set(1, username)
+    return True
 
 
 if __name__ == '__main__':
