2dbea9ecc315d699e6a780fe5e6e8e22dc069f91
KristinaU <urosova.k@gmail.com>
2020-06-07 08:08:18 +0100

    Implement token generation as a randomised alphanumerical string

--- app.py
+++ app.py
@@ -4,28 +4,32 @@ from flask import request
 import redis
 import json
 from flask import jsonify
+import random
+import string
 
 app = Flask(__name__)
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 Users = {'id', 'username', 'password'}
 
+
 @app.route('/')
-def hello_world():
-    return "Hello"
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 
 
 # registration
 @app.route('/registration', methods=['POST'])
 def registration():
     if create_user(request.form['id'], request.form['username'], request.form['password']):
-        print (r.hvals('Users'))
+        print(r.hvals('Users'))
         return 'yeah haaahhh', 200
 
-#        if r.get(1) == 'username':
-#            return 'yeah haaahhh', 200
-#        else:
-#            return 'Something went wrong', 400
     else:
         return 'Ohh nooo', 400
 
@@ -35,11 +39,29 @@ def create_user(id, username, password):
     r.hmset('Users', new_user)
     return True
 
+
 # list of all users
 @app.route('/users', methods=['GET'])
 def users():
-    print (r.hgetall('Users'))
+    print(r.hgetall('Users'))
     return jsonify(str(r.hvals('Users')))
+
+# login functionality
+@app.route('/login', methods=['POST'])
+def login():
+
+    letters = 'abcdefghyjklmnopqrstuvwxyz1234567890'
+
+    if check_user(request.form['username'], request.form['password']):
+        return ''.join(random.choice(letters) for i in range(32))
+    else:
+        return 400
+
+def check_user(username, password):
+    if username is not None and password is not None:
+        return True, 200
+    else:
+        return False, 400
 
 
 if __name__ == '__main__':
